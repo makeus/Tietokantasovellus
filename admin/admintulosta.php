129378361f2f6f8f3b8d18ccea29835cb3289dc1
Mathias Keus <noclip@suomi24.fi>
2012-03-27 20:18:21 +0300

    taulu fixi, admintulostusalku

--- admin/admintulosta.php
+++ admin/admintulosta.php
@@ -4,9 +4,7 @@
    if($_SESSION["käyttäjänimi"] == 'admin'){
 
     include("../yhteys.php");
-    $kysely = $yhteys->prepare("SELECT * FROM Viesti");
-    $kysely->execute();
- 
+    $kysely = pg_query($yhteys, "SELECT * FROM Viesti");
     echo "<table>
            <tr>
              <th>ID</th>
@@ -15,21 +13,15 @@
              <th>Teksti</th>
              <th>Poista</th>
            </tr>";
-    while ($rivi = $kysely->fetch()) {
+    while ($rivi = pg_fetch_array($kysely)) {
      echo "<tr>
              <td>" . $rivi["id"] . "</td>
              <td>" . $rivi["aika"] . "</td>
-             <td>" . $rivi["kirjoittaja"] . "</td>
              <td>" . $rivi["teksti"] . "</td>
-             <td> <a href=poista.php?id=" . $rivi["id"] . ">x</a></td>
+             <td> <a href=poistaRyhma.php?id=" . $rivi["id"] . ">x</a></td>
            </tr>";
     }
     echo "</table><br>";
-    echo '<form action="laheta.php" method="post">
-            <input type="text" name="viesti" />
-            <input type="submit" value="Lähetä" />
-          </form>
-          <p><a href="logout.php">Kirjaudu ulos!</a></p>';
   }
   else { 
     header('HTTP/1.1 403 Forbidden');
