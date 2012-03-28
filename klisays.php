893fd821d4a7466666b33ee1590678c660f61d20
Julius Räihä <julius.raiha@helsinki.fi>
2012-03-28 16:57:06 +0300

    nyt toimii klis

--- klisays.php
+++ klisays.php
@@ -2,6 +2,6 @@
   session_start();
   include("yhteys.php");
   $kysely = pg_prepare($yhteys, "lisays", 'INSERT INTO Käyttäjä (Käyttäjänimi, Sähköposti, Salasana, Ylläpitäjä) VALUES ($1, $2, $3, $4)');
-  $kysely = pg_execute($yhteys, "lisays", array($_POST["kayttajanimi"], $_POST["sahkoposti"], $_SESSION["salasana"], $_POST["admin"]));
+  $kysely = pg_execute($yhteys, "lisays", array($_POST["kayttajanimi"], $_POST["sahkoposti"], $_POST["salasana"], $_POST["admin"]));
   header("Location: http://jeraiha.users.cs.helsinki.fi");
 ?>
