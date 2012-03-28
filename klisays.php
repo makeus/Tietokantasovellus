810bf5ab7712db691351cfbe79cc9bb58c9faced
Julius Räihä <julius.raiha@helsinki.fi>
2012-03-28 16:53:43 +0300

    asdsd

--- klisays.php
+++ klisays.php
@@ -1,7 +1,7 @@
 <?php
   session_start();
   include("yhteys.php");
-  $kysely = pg_prepare($yhteys, "lisays", 'INSERT INTO Käyttäjä (Käyttäjänimi, Sähköposti, Salasana, ylläpitäjä) VALUES (NOW(), $1, $2, $3, $4)');
-  $kysely = pg_execute($yhteys, "lisays", array($_POST["knimi"], $_POST["sposti"], $_SESSION["ssana"], $_POST["admin"]));
+  $kysely = pg_prepare($yhteys, "lisays", 'INSERT INTO Käyttäjä (Käyttäjänimi, Sähköposti, Salasana, Ylläpitäjä) VALUES ($1, $2, $3, $4)');
+  $kysely = pg_execute($yhteys, "lisays", array($_POST["kayttajanimi"], $_POST["sahkoposti"], $_SESSION["salasana"], $_POST["admin"]));
   header("Location: http://jeraiha.users.cs.helsinki.fi");
 ?>
--- lisaakayttaja.php
+++ lisaakayttaja.php
@@ -4,9 +4,9 @@
 </head>
 <body>
 <form name="käyttäjän lisäys" action="klisays.php" method="post" >
-Käyttäjänimi:<input type="text" name="knimi" required placeholder="Käyttäjänimi" /><br>
-Sähköposti:<input type="text" name="sposti" required placeholder="Sähköposti" /><br>
-Salasana:<input type="password" name="ssana" required placeholder="Salasana"/><br>
+Käyttäjänimi:<input type="text" name="kayttajanimi" required placeholder="Käyttäjänimi" /><br>
+Sähköposti:<input type="text" name="sahkoposti" required placeholder="Sähköposti" /><br>
+Salasana:<input type="password" name="salasana" required placeholder="Salasana"/><br>
 Uusi ylläpitäjä? <input type="boolean" name="admin" required placeholder="T/F" />
 <input type="submit" />
 </form>
