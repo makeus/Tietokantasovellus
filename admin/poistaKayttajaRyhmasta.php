23538cad656243def0a559b4dca1b0a6197ad766
Mathias Keus <noclip@suomi24.fi>
2012-03-28 22:10:55 +0300

    salee taas iha sekasi....GIT!!!

--- admin/poistaKayttajaRyhmasta.php
+++ admin/poistaKayttajaRyhmasta.php
@@ -7,11 +7,7 @@
     $ryhmanimi = $_POST["ryhmannimi"];
     $ryhmat = pg_query($yhteys, "SELECT id FROM Ryhmä where ryhmännimi=('$ryhmanimi')");
     $rivi = pg_fetch_row($ryhmat);
-<<<<<<< HEAD
-    header("Location: admin.php?p=1&m=$rivi[0]");
-=======
     header("Location: admin/admin.php?p=1&m=$rivi[0]");
->>>>>>> ecffd3d61666b4284e5a28799cf407fb169f5b32
   } else {
     header('HTTP/1.1 403 Forbidden');
   }
