3e3961b24570d3a52599f89b14f6852b77bba82a
Mathias Keus <noclip@suomi24.fi>
2012-04-01 21:34:53 +0300

    jotai pientä siellä täällä

--- admin/lisaakayttaja.php
+++ admin/lisaakayttaja.php
@@ -1,7 +1,7 @@
 <form name="Luo käyttäjä" action="klisays.php" method="post" ><pre>
-Käyttäjänimi:	<input type="text" autofocus name="kayttajanimi" required placeholder="Käyttäjänimi" />
+Käyttäjänimi:	<input type="text" autofocus name="kayttajanimi" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$" required placeholder="Käyttäjänimi" />
 Sähköposti:	<input type="text" name="sahkoposti" placeholder="Sähköposti" />
-Salasana:	<input type="password" name="salasana" required placeholder="Salasana"/>
+Salasana:	<input type="password" name="salasana" required placeholder="Salasana" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" />
 Ylläpitäjä?	<select name="admin">
   <option value="t" selected>Kyllä</option>
   <option value="f">Ei</option>
