bfa0966c5403e0ab08afa3fd66eed0da0ca2d934
Mathias Keus <noclip@suomi24.fi>
2012-04-03 14:33:39 +0300

    muotoilua

--- admin/uusiKategoria.php
+++ admin/uusiKategoria.php
@@ -2,6 +2,6 @@
   session_start();
   include("../yhteys.php");
   $kysely = pg_prepare($yhteys, "lisays", 'INSERT INTO Kategoria (Kategoriannimi, Näkyvyys) VALUES ($1, $2)');
-  $kysely = pg_execute($yhteys, "lisays", array($_POST["nimi"], $_POST["nakyvyys"]));
+  $kysely = pg_execute($yhteys, "lisays", array(htmlspecialchars($_POST["nimi"]), $_POST["nakyvyys"]));
   header("Location: admin.php?p=5");
 ?>
--- lisaaViesti.php
+++ lisaaViesti.php
@@ -1,18 +1,18 @@
 <?php
   session_start();
   include("yhteys.php");
-  $kayttajanimi = $_SESSION["käyttäjänimi"];
+  $kayttajanimi = htmlspecialchars($_SESSION["käyttäjänimi"]);
   if(isset($_POST["vastaus"])) {
     $kysely = pg_prepare($yhteys, "lisays", 'INSERT INTO Viesti (Aika, Otsikko, Teksti, Kategoria, Viestinlukeneet, Kirjoittaja, Vastaus) 
                                                   VALUES (NOW(), $1, $2, $3, Array[$4], $5, $6)');
-    $kysely = pg_execute($yhteys, "lisays", array($_POST["otsikko"], $_POST["teksti"], $_POST["kategoria"], $kayttajanimi, $kayttajanimi, $_POST["vastaus"]));
+    $kysely = pg_execute($yhteys, "lisays", array(htmlspecialchars($_POST["otsikko"]), htmlspecialchars($_POST["teksti"]), $_POST["kategoria"], $kayttajanimi, $kayttajanimi, $_POST["vastaus"]));
     header("Location:/");
   } 
   else {
 
   $kysely = pg_prepare($yhteys, "lisays", 'INSERT INTO Viesti (Aika, Otsikko, Teksti, Kategoria, Viestinlukeneet, Kirjoittaja) 
                                                   VALUES (NOW(), $1, $2, $3, Array[$4], $5)');
-  $kysely = pg_execute($yhteys, "lisays", array($_POST["otsikko"], $_POST["teksti"], $_POST["kategoria"], $kayttajanimi, $kayttajanimi));
+  $kysely = pg_execute($yhteys, "lisays", array(htmlspecialchars($_POST["otsikko"]), htmlspecialchars($_POST["teksti"]), $_POST["kategoria"], $kayttajanimi, $kayttajanimi));
   header("Location:/");
   }
 ?>
