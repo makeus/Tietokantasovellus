857d8f4434f5266afaca5dd7361673f129fc7f22
Julius Räihä <julius.raiha@helsinki.fi>
2012-04-03 13:40:03 +0300

    asds

--- tulostaViesti.php
+++ tulostaViesti.php
@@ -3,12 +3,20 @@ function tulostaViesti($viestin_id){
   session_start(); 
   echo "<div>";
   printtaaViesti($viestin_id);
-  include("../yhteys.php");
-  $viestit = pg_prepare($yhteys, "haku" ,'SELECT Id,Vastaus FROM Viesti WHERE Id= $1 order by aika');
+  include("yhteys.php");
+  $viestit = pg_prepare($yhteys, "haku" ,'SELECT Id FROM Viesti WHERE Vastaus= $1 order by aika');
   $viestit = pg_execute($yhteys, "haku", array($viestin_id));
   while ($rivi = pg_fetch_array($viestit)){
-    tulostaViesti($rivi["Vastaus"]);
+    tulostaViesti($rivi["Id"]);
   }
   echo "</div>";
 }
+function printtaaViesti($id){
+  include("yhteys.php");
+  $viesti = pg_prepare($yhteys, "haku" ,'SELECT Teksti FROM Viesti WHERE Id= $1');
+  $viesti = pg_execute($yhteys, "haku", array($id));
+  $rivi = pg_fetch_array($viesti);
+  echo "".$rivi["Teksti"]."";
+}
+
 ?>
