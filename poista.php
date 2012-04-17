290a3306452339eb9098e254e16e206fb38d0272
Mathias Keus <keus@keus-desktop>
2012-04-17 15:46:00 +0300

    fdgdfg

--- poista.php
+++ poista.php
@@ -2,7 +2,11 @@
 
 session_start();
 include("yhteys.php");
-$kysely = pg_prepare($yhteys, "lahetys", 'DELETE FROM Viesti WHERE id=($1)');
-$kysely = pg_execute($yhteys, "lahetys", array($_GET["id"]));
+$viesti = pg_query_params($yhteys, 'SELECT Kirjoittaja FROM Viesti WHERE Id= $1', array($_GET["id"]));
+$kirjoittaja = pg_fetch_array($viesti);
+if ($_SESSION["admin"] == 't' || $kirjoittaja[0] == $_SESSION["käyttäjänimi"]) {
+    $kysely = pg_prepare($yhteys, "lahetys", 'DELETE FROM Viesti WHERE id=($1)');
+    $kysely = pg_execute($yhteys, "lahetys", array($_GET["id"]));
+}
 header("Location: index.php");
 ?>
