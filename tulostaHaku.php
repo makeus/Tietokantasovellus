89f789bd7513e9d6f276b825de6d6aa681e4f1f8
Julius Räihä <julius.raiha@helsinki.fi>
2012-04-21 20:04:31 +0300

    haku valmis?

--- tulostaHaku.php
+++ tulostaHaku.php
@@ -7,6 +7,9 @@ if (!session_is_registered("käyttäjänimi")) {
     include("logiikka/kategoriafunktiot.php");
     include("logiikka/viestifunktiot.php");
 
+    /*
+     * Tarkistaa, että käyttäjä saa nähdä hakutuloksena tulleen viestin ja kutsuu printaaViesti()
+     */
     $kayttajanimi = $_SESSION["käyttäjänimi"];
     $nakyvyys = getNakyvyys($kayttajanimi);
 
@@ -14,23 +17,27 @@ if (!session_is_registered("käyttäjänimi")) {
     $hakusana = escape($_POST["hakusana"]);
     $hakutulokset = getHakutulokset($hakunimi,$hakusana);
     echo "<br/>";
+    echo "<div  id=\"viestijavastauksetlaatikko\">";
     if (!empty($hakutulokset)) {
         foreach ($hakutulokset as $rivi) {
             if ((!empty($rivi)) && (in_array($rivi["kategoria"], $nakyvyys))) {
-                echo "<div class=\"hakutulos\">";
                 echo printtaaViesti($rivi["id"],$rivi["id"]);
-                echo "</div>";
             }
         }
     }else{
         echo "<p>Ei tuloksia</p>";
     }
-    
+    echo "</div>";
 }
+
+/*
+ * Tulostaa hakutuloksena tulleen viestin.
+ */
 
 
 function printtaaViesti($id, $eka) {
     $viesti = getViesti($id);
+    echo "  <p class=\"kategoria\">" . $viesti["otsikko"] . "</p>";
     echo "<p class=\"Kirjoittaja\"><strong>" . $viesti["kirjoittaja"] . "</strong> (" . date("d.m.y H:i:s", strtotime($viesti["aika"])) . ") " . $viesti["otsikko"];
     if ($_SESSION["admin"] == 't' || $viesti["kirjoittaja"] == $_SESSION["käyttäjänimi"]) {
         echo "<a class=\"poistolink\" href=# onclick='varmista(\"toiminnot/poistaViesti.php?id=" . $id . "\", \"Oletko varma, että haluat poistaa viestin?\")'>poista</a></p>";
